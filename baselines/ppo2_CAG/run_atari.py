3f21668e117d1a49e498a8107ac7dcfb0be3235b
wjh720 <wjh720.eric@gmail.com>
2017-12-28 14:36:49 +0800

    Wee

--- baselines/ppo2_CAG/run_atari.py
+++ baselines/ppo2_CAG/run_atari.py
@@ -51,7 +51,7 @@ def main():
     parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
-    parser.add_argument('--num-timesteps', type=int, default=int(100))
+    parser.add_argument('--num-timesteps', type=int, default=int(10000))
     args = parser.parse_args()
     logger.configure()
     train(args.env, num_timesteps=args.num_timesteps, seed=args.seed,
