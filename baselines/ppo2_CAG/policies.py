653fb57e049f794a73e90bd06cc7049b7f8edd81
wjh720 <wjh720.eric@gmail.com>
2017-12-28 14:33:57 +0800

    Awe

--- baselines/ppo2_CAG/policies.py
+++ baselines/ppo2_CAG/policies.py
@@ -99,13 +99,13 @@ class CnnPolicy(object):
         nh, nw, nc = ob_space.shape
         ob_shape = (nbatch, nh, nw, nc)
         nact = ac_space.n
-        X = tf.placeholder(tf.uint8, ob_shape) #obs
+        X = tf.placeholder(tf.float32, ob_shape) #obs
         with tf.variable_scope("model", reuse=reuse):
-            h = conv(tf.cast(X, tf.float32)/255., 'c1', nf=32, rf=8, stride=4, init_scale=np.sqrt(2))
-            h2 = conv(h, 'c2', nf=64, rf=4, stride=2, init_scale=np.sqrt(2))
-            h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
+            h = conv(X, 'c1', nf=32, rf=3, stride=1, init_scale=np.sqrt(2), pad="SAME")
+            h2 = conv(h, 'c2', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), pad="SAME")
+            h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), pad="SAME")
             h3 = conv_to_fc(h3)
-            h4 = fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2))
+            h4 = fc(h3, 'fc1', nh=128, init_scale=np.sqrt(2))
             pi = fc(h4, 'pi', nact, act=lambda x:x, init_scale=0.01)
             vf = fc(h4, 'v', 1, act=lambda x:x)[:,0]
 
--- baselines/ppo2_CAG/run_atari.py
+++ baselines/ppo2_CAG/run_atari.py
@@ -50,7 +50,7 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='lnlstm')
+    parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
     parser.add_argument('--num-timesteps', type=int, default=int(100))
     args = parser.parse_args()
     logger.configure()
