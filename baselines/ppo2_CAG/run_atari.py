d1558963b83de733a7ac65d48ba5d3c68e270efe
wjh720 <wjh720.eric@gmail.com>
2017-12-29 19:30:52 +0800

    21

--- baselines/ppo2_CAG/run_atari.py
+++ baselines/ppo2_CAG/run_atari.py
@@ -39,7 +39,7 @@ def train(env_id, num_timesteps, seed, policy):
     #env = VecFrameStack(env, 4)
     policy = {'cnn' : CnnPolicy, 'lstm' : LstmPolicy, 'lnlstm' : LnLstmPolicy}[policy]
     print('ab')
-    ppo2.learn(policy=policy, env=env, nsteps=64, nminibatches=1,
+    ppo2.learn(policy=policy, env=env, nsteps=32, nminibatches=1,
         lam=0.95, gamma=0.99, noptepochs=10, log_interval=1, save_interval = 100,
         ent_coef=.01,
         lr=lambda f : f * 2.5e-4,
