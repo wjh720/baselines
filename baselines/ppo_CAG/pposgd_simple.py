a55e79bef8313cb7ce77b4557a8d88ee946f0d23
wjh720 <wjh720.eric@gmail.com>
2017-12-30 18:52:02 +0800

    123

--- baselines/ppo_CAG/pposgd_simple.py
+++ baselines/ppo_CAG/pposgd_simple.py
@@ -152,7 +152,7 @@ def learn(env, policy_func, *,
     if (flag_load > 0):
         print('Loading!')
         time.sleep(3)
-        data_path = '/home/icenter/tmp/openai_data/para_%i/' % (flag_load / 100)
+        data_path = '/home/icenter/tmp/openai_data/para_%i_%i/' % (flag_load // 100, flag_load // 10)
         U.load_state(data_path + 'para')
 
     while True:
@@ -176,7 +176,7 @@ def learn(env, policy_func, *,
         logger.log("********** Iteration %i ************"%iters_so_far)
 
         if (iters_so_far % 10 == 0):
-            data_path = '/home/icenter/tmp/openai_data/para_%i/' % (iters_so_far / 100)
+            data_path = '/home/icenter/tmp/openai_data/para_%i_%i/' % (iters_so_far // 100, iters_so_far // 10)
             U.save_state(data_path + 'para')
 
 
